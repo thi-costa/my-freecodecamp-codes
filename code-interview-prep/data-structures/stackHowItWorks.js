4f7feb9b3de6b0c82828e000910ed523ae3e6f66
thi-costa <thiago14abr@gmail.com>
2022-03-12 15:12:55 -0300

    Code that represent stacks funcionalities

---
+++ code-interview-prep/data-structures/stackHowItWorks.js
@@ -0,0 +1,13 @@
+/*
+This code show how a stack works using array pre-built
+methods.
+*/
+let homeworkStack = ["BIO12", "HIS80", "MAT122", "PSY44"];
+// Only change code below this line
+console.log(homeworkStack);
+
+homeworkStack.pop();
+console.log(homeworkStack);
+
+homeworkStack.push("CS50");
+console.log(homeworkStack);
